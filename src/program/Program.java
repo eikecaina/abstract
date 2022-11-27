01490c6c5a48d185e22731e8e22e4f385fc10f36
Eike Hank <cainaeike@hotmail.com>
2022-11-27 14:10:03 -0300

    Classes abstratas

---
+++ src/program/Program.java
@@ -0,0 +1,61 @@
+package program;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entites.Company;
+import entites.Individual;
+import entites.TaxPayer;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<TaxPayer> list = new ArrayList<>();
+		
+		System.out.print("Enter the number of tax payers: ");
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Tax payer #" + i + " data: ");
+			System.out.print("Individual or company (i/c)?: ");
+			char selector = sc.next().charAt(0);
+			sc.nextLine();
+			System.out.print("Name: ");
+			String name = sc.nextLine();
+			System.out.print("Anual Income: ");
+			double anualIncome = sc.nextDouble();
+			
+			if (selector == 'i') {
+				System.out.print("Health expenditures: ");
+				double healthExpenditures = sc.nextDouble();
+				list.add(new Individual(name, anualIncome, healthExpenditures));
+			} else {
+				System.out.print("Number of employees: ");
+				int employees = sc.nextInt();
+				list.add(new Company(name, anualIncome, employees));
+			}
+			
+		}
+		
+		double sum = 0.0;
+		System.out.println();
+		System.out.println("TAXES PAID:");
+		for (TaxPayer tp : list) {
+			double tax = tp.tax();
+			System.out.println(tp.getName() + ": $ " + String.format("%.2f", tax));
+			sum += tax;
+		}
+		
+		System.out.println();
+		System.out.println("TOTAL TAXES: $ " + String.format("%.2f", sum));
+		
+		sc.close();
+	}
+
+}
